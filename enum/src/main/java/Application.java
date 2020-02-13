e169c993fb97d2b81a1bed6439e0ab8d6f2e604c
zkj <438101845@qq.com>
2020-02-13 17:23:35 +0800

    [zkj]-course3-finished

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,23 @@
+import modeule.Gender;
+import modeule.Student;
+
 public class Application {
 
   public static void main(String[] args) {
+    Student student1 = new Student("Linda", Gender.valueOf(Gender.class, "FEMALE"));
+    Student student2 = new Student("Bob", Gender.valueOf(Gender.class, "MALE"));
+    Student student3 = new Student("Solider", Gender.valueOf(Gender.class, "MALE"));
+    Student student4 = new Student("Cindy", Gender.valueOf(Gender.class, "FEMALE"));
+    Student[] students = new Student[] {student1, student2, student3, student4};
 
+    findFemaleStudents(students);
   }
 
+  public static void findFemaleStudents(Student[] students) {
+    for (Student student : students) {
+      if (student.getGender().getGender().equals("Female")) {
+        System.out.println("女学生姓名为" + student.getName());
+      }
+    }
+  }
 }
---
+++ enum/src/main/java/modeule/Student.java
@@ -0,0 +1,30 @@
+package modeule;
+
+public class Student {
+    private String name;
+    private Gender gender;
+
+    public Student() {
+    }
+
+    public Student(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
